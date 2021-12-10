aba729e8822a1d8e5c839f31dab112bf1202ca79
wfj <wufangjie1223@126.com>
2021-12-10 14:38:04 +0800

    unsafe linked list (label pre (by vec), linked next)

---
+++ aoc2020/src/day23.rs
@@ -0,0 +1,169 @@
+use std::collections::HashSet;
+use std::ptr;
+
+pub fn part1() -> usize {
+    //let mut cups = Cups::new(vec![3, 8, 9, 1, 2, 5, 4, 6, 7]);
+    let mut cups = Cups::new(vec![7, 8, 4, 2, 3, 5, 9, 1, 6]);
+    for _ in 0..100 {
+        cups.next_move();
+    }
+    cups.calc_res()
+}
+
+#[derive(Debug)]
+struct Cups {
+    data: Vec<usize>,
+    n: usize,
+    destination: usize,
+}
+
+impl Cups {
+    fn new(data: Vec<usize>) -> Self {
+        let n = data.len();
+        Self {
+            data,
+            n,
+            destination: 0,
+        }
+    }
+
+    fn find_destination(&mut self) {
+        let mut v_max_less = 0;
+        let mut v_max_all = 0;
+        let mut i_max_less = 0;
+        let mut i_max_all = 0;
+        for i in 4..self.n {
+            if self.data[i] < self.data[0] {
+                if self.data[i] > v_max_less {
+                    v_max_less = self.data[i];
+                    i_max_less = i;
+                }
+            } else if self.data[i] > v_max_all {
+                v_max_all = self.data[i];
+                i_max_all = i;
+            }
+        }
+        if v_max_less == 0 {
+            self.destination = i_max_all;
+        } else {
+            self.destination = i_max_less;
+        }
+    }
+
+    fn place_and_next(&mut self) {
+        let mut new = vec![];
+        for i in 4..self.n {
+            new.push(self.data[i]);
+            if i == self.destination {
+                for j in 1..4 {
+                    new.push(self.data[j]);
+                }
+            }
+        }
+        new.push(self.data[0]);
+        self.data = new;
+    }
+
+    fn next_move(&mut self) {
+        self.find_destination();
+        self.place_and_next();
+    }
+
+    fn calc_res(&self) -> usize {
+        self.data
+            .iter()
+            .chain(self.data.iter())
+            .skip_while(|&&x| x != 1)
+            .skip(1)
+            .take(self.n - 1)
+            .fold(0, |acc, b| acc * 10 + b)
+    }
+}
+
+const N: usize = 1000000;
+const M: usize = 10000000;
+
+pub fn part2() -> usize {
+    let mut cups = Vec::with_capacity(N);
+    for i in 1..=N {
+        cups.push(Cup::new(i));
+    }
+    for i in 9..N - 1 {
+        cups[i].next = &mut cups[i + 1] as *mut Cup;
+    }
+
+    let left = [N, 7, 8, 4, 2, 3, 5, 9, 1, 6, 10];
+    //let left = [N, 3, 8, 9, 1, 2, 5, 4, 6, 7, 10];
+    for i in 0..left.len() - 1 {
+        cups[left[i] - 1].next = &mut cups[left[i + 1] - 1] as *mut Cup;
+    }
+
+    let mut cur = left[1] - 1;
+    for _ in 0..M {
+        let (p3, picked) = cups[cur].remove_next_3();
+        let mut i = cur;
+        loop {
+            if i == 0 {
+                i = N;
+            }
+            i -= 1;
+            if !picked.contains(&i) {
+                cups[i].insert_3(p3);
+                break;
+            }
+        }
+        cur = cups[cur].find_next_cur();
+    }
+
+    unsafe { (*cups[0].next).label * (*(*cups[0].next).next).label }
+}
+
+struct Cup {
+    label: usize,
+    next: *mut Cup,
+}
+
+impl Cup {
+    fn new(label: usize) -> Self {
+        Self {
+            label,
+            next: ptr::null_mut(),
+        }
+    }
+
+    fn remove_next_3(&mut self) -> (*mut Self, HashSet<usize>) {
+        let mut picked = HashSet::new();
+        let ret = self.next;
+        unsafe {
+            let mut next = self.next;
+            for _ in 0..3 {
+                picked.insert((*next).label - 1);
+                next = (*next).next;
+            }
+            self.next = next;
+        }
+        (ret, picked)
+    }
+
+    fn insert_3(&mut self, p: *mut Self) {
+        unsafe {
+            (*(*(*p).next).next).next = self.next;
+            self.next = p;
+        }
+    }
+
+    fn find_next_cur(&self) -> usize {
+        unsafe { (*self.next).label - 1 }
+    }
+
+    fn print_next_n(&self, n: usize) {
+        let mut p = self;
+        unsafe {
+            for i in 0..n {
+                print!("{} -> ", p.label);
+                p = &*p.next;
+            }
+        }
+        println!("");
+    }
+}
--- aoc2020/src/main.rs
+++ aoc2020/src/main.rs
@@ -2,25 +2,10 @@ use std::fs::{self, File};
 use std::io::{self, BufRead, Read};
 use std::path::Path;
 
-// mod day1;
-// mod day2;
-// mod day3;
-// mod day4;
-// mod day5;
-// mod day6;
-// mod day7;
-// mod day8;
-// mod day17;
-// mod day9;
-// mod day16;
-// mod day18;
-// mod day15;
-// mod day19;
-// mod day19_no_size_hint;
+mod day23;
 
 fn main() {
-    dbg!(day19::part1());
-    dbg!(day19::part2());
+    dbg!(day23::part2());
 }
 
 fn read_lines<P>(filename: P) -> Vec<String>
