e3b2ff018b455d52e577968c8732b362f72bc88d
wfj <wufangjie1223@126.com>
2021-12-15 14:49:30 +0800

    the dp answer is wrong

--- aoc2021/src/day15.rs
+++ aoc2021/src/day15.rs
@@ -1,27 +1,37 @@
 use crate::read_lines;
-
-pub fn part1() -> usize {
-    let lines: Vec<Vec<u8>> = read_lines("./data/day15.txt")
-        .into_iter()
-        .map(|line| line.bytes().map(|x| x - b'0').collect())
-        .collect();
-    let nrow = lines.len();
-    let ncol = lines[0].len();
-    let mut dp = Vec::with_capacity(ncol);
-    dp.push(0);
-    for j in 1..ncol {
-        dp.push(dp[j - 1] + lines[0][j] as usize);
-    }
-    for i in 1..nrow {
-        dp[0] += lines[i][0] as usize;
-        for j in 1..ncol {
-            dp[j] = dp[j - 1].min(dp[j]) + lines[i][j] as usize;
-        }
-    }
-    dp[ncol - 1]
-}
+use utils::Heap;
 
 const N: usize = 5;
+
+pub fn part1() -> usize {
+    // const N: usize = 1;
+    0
+}
+
+// NOTE: the DP answer is wrong: left, up is possible
+// pub fn part2() -> usize {
+//     let lines: Vec<Vec<usize>> = read_lines("./data/15.txt")
+//         .into_iter()
+//         .map(|line| line.bytes().map(|x| (x - b'0') as usize).collect())
+//         .collect();
+//     let nrow = lines.len();
+//     let ncol = lines[0].len();
+
+//     let find_real_risk = |i: usize, j: usize| -> usize {
+//         let ret = (lines[i % nrow][j % ncol] + i / nrow + j / ncol);
+//         ret % 10 + ret / 10
+//     };
+
+//     let mut dp = vec![vec![usize::MAX; ncol * N]; nrow];
+//     dp[0] = 0;
+//     for i in 0..nrow * N {
+//         dp[0] += find_real_risk(i, 0);
+//         for j in 1..ncol * N {
+//             dp[j] = dp[j - 1].min(dp[j]) + find_real_risk(i, j);
+//         }
+//     }
+//     dp[ncol * N - 1] - lines[0][0]
+// }
 
 pub fn part2() -> usize {
     let lines: Vec<Vec<usize>> = read_lines("./data/day15.txt")
@@ -36,15 +46,45 @@ pub fn part2() -> usize {
         ret % 10 + ret / 10
     };
 
-    let mut dp = vec![usize::MAX; ncol * N];
-    dp[0] = 0;
-    for i in 0..nrow * N {
-        dp[0] += find_real_risk(i, 0);
-        for j in 1..ncol * N {
-            dp[j] = dp[j - 1].min(dp[j]) + find_real_risk(i, j);
+    let nrowN = nrow * N;
+    let ncolN = ncol * N;
+
+    let mut lowest = vec![vec![0; ncolN]; nrowN];
+    lowest[0][0] = 1;
+    let mut heap = Heap::new();
+    heap.push((0, 0, 0));
+    while let Some((d, i, j)) = heap.pop() {
+        if i == nrowN - 1 && j == ncolN - 1 {
+            return d;
+        }
+
+        for ii in if i == 0 {
+            0..=1
+        } else if i < nrowN - 1 {
+            i - 1..=i + 1
+        } else {
+            i - 1..=i
+        } {
+            if lowest[ii][j] == 0 {
+                let d2 = d + find_real_risk(ii, j);
+                lowest[ii][j] = d2;
+                heap.push((d2, ii, j));
+            }
+        }
+
+        for jj in if j == 0 {
+            0..=1
+        } else if j < ncolN - 1 {
+            j - 1..=j + 1
+        } else {
+            j - 1..=j
+        } {
+            if lowest[i][jj] == 0 {
+                let d2 = d + find_real_risk(i, jj);
+                lowest[i][jj] = d2;
+                heap.push((d2, i, jj));
+            }
         }
     }
-    dp[ncol * N - 1] - lines[0][0]
+    unreachable!();
 }
-
-// 2948
