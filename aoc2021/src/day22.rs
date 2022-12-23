78a76cdf3accce4dd79cd0dcfa11ca025b118a6f
wfj <wufangjie1223@126.com>
2022-12-23 23:16:04 +0800

    change algorithm: overlap + backtrace?

--- aoc2021/src/day22.rs
+++ aoc2021/src/day22.rs
@@ -1,6 +1,105 @@
 use crate::read_lines;
 use regex::Regex;
-use std::collections::HashSet;
+//use std::cmp::Ordering::{Equal, Greater, Less};
+//use std::collections::HashSet;
+
+type T = i64;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Cube {
+    on: bool,
+    x1: T,
+    x2: T,
+    y1: T,
+    y2: T,
+    z1: T,
+    z2: T,
+}
+
+impl Cube {
+    pub fn new(on: bool, x1: T, x2: T, y1: T, y2: T, z1: T, z2: T) -> Self {
+        Self {
+            on,
+            x1,
+            x2,
+            y1,
+            y2,
+            z1,
+            z2,
+        }
+    }
+
+    pub fn from_sp(vec: Vec<T>, on: bool) -> Self {
+        Self {
+            on,
+            x1: vec[0],
+            x2: vec[1] + 1,
+            y1: vec[2],
+            y2: vec[3] + 1,
+            z1: vec[4],
+            z2: vec[5] + 1,
+        }
+    }
+
+    pub fn count(&self) -> T {
+        (self.x2 - self.x1) * (self.y2 - self.y1) * (self.z2 - self.z1)
+    }
+
+    // self 是后来的, 算交集需要的话拆分, 返回值是(明暗变化, 拆分后的)
+    pub fn intersect(&self, other: &Self) -> (T, Vec<Cube>) {
+        let xmin = self.x1.max(other.x1);
+        let xmax = self.x2.min(other.x2);
+        if xmin >= xmax {
+            return (0, vec![*self]);
+        }
+        let ymin = self.y1.max(other.y1);
+        let ymax = self.y2.min(other.y2);
+        if ymin >= ymax {
+            return (0, vec![*self]);
+        }
+        let zmin = self.z1.max(other.z1);
+        let zmax = self.z2.min(other.z2);
+        if zmin >= zmax {
+            return (0, vec![*self]);
+        }
+
+        let mut left_lst = vec![];
+        if self.x1 < xmin {
+            left_lst.push(Self::new(
+                self.on, self.x1, xmin, self.y1, self.y2, self.z1, self.z2,
+            ));
+        }
+        if self.x2 > xmax {
+            left_lst.push(Self::new(
+                self.on, xmax, self.x2, self.y1, self.y2, self.z1, self.z2,
+            ));
+        }
+        if self.y1 < ymin {
+            left_lst.push(Self::new(
+                self.on, xmin, xmax, self.y1, ymin, self.z1, self.z2,
+            ));
+        }
+        if self.y2 > ymax {
+            left_lst.push(Self::new(
+                self.on, xmin, xmax, ymax, self.y2, self.z1, self.z2,
+            ));
+        }
+        if self.z1 < zmin {
+            left_lst.push(Self::new(self.on, xmin, xmax, ymin, ymax, self.z1, zmin));
+        }
+        if self.z2 > zmax {
+            left_lst.push(Self::new(self.on, xmin, xmax, ymin, ymax, zmax, self.z2));
+        }
+
+        if self.on == other.on {
+            (0, left_lst)
+        } else if self.on {
+            ((xmax - xmin) * (ymax - ymin) * (zmax - zmin), left_lst)
+        } else {
+            (-(xmax - xmin) * (ymax - ymin) * (zmax - zmin), left_lst)
+        }
+    }
+}
 
 pub fn part1() -> usize {
     let lines = read_lines("./data/day22.txt");
@@ -9,105 +108,52 @@ pub fn part1() -> usize {
     )
     .unwrap();
     let n = lines.len();
-    let mut state_lst = Vec::with_capacity(n);
-    let mut cube_lst: Vec<Vec<i32>> = Vec::with_capacity(n);
+    let mut cube_lst: Vec<Cube> = Vec::with_capacity(n);
     for line in lines {
         let caps = re.captures(&line).unwrap();
-        state_lst.push(if caps.get(1).unwrap().as_str() == "on" {
-            true
-        } else {
-            false
-        });
-        cube_lst.push(
-            (2..8)
-                .into_iter()
-                .map(|i| caps.get(i).unwrap().as_str().parse().unwrap())
-                .collect(),
-        );
-    }
-
-    let mut xset = HashSet::new();
-    let mut yset = HashSet::new();
-    let mut zset = HashSet::new();
-
-    xset.insert(-50);
-    xset.insert(51);
-    yset.insert(-50);
-    yset.insert(51);
-    zset.insert(-50);
-    zset.insert(51);
-
-    for cube in &cube_lst {
-        for x in [cube[0], cube[1] + 1] {
-            if x > -50 && x < 51 {
-                xset.insert(x);
+        let state = caps.get(1).unwrap().as_str() == "on";
+        let mut pos: Vec<T> = (2..8)
+            .into_iter()
+            .map(|i| caps.get(i).unwrap().as_str().parse::<T>().unwrap())
+            .collect();
+        let mut need = true;
+        for i in [0, 2, 4] {
+            pos[i] = pos[i].max(-50);
+            pos[i + 1] = pos[i + 1].min(50);
+            if pos[i + 1] < pos[i] {
+                need = false;
+                break;
             }
         }
-        for x in [cube[2], cube[3] + 1] {
-            if x > -50 && x < 51 {
-                yset.insert(x);
-            }
-        }
-        for x in [cube[4], cube[5] + 1] {
-            if x > -50 && x < 51 {
-                zset.insert(x);
-            }
-        }
-    }
-
-    let mut xlst: Vec<i32> = xset.into_iter().collect();
-    let mut ylst: Vec<i32> = yset.into_iter().collect();
-    let mut zlst: Vec<i32> = zset.into_iter().collect();
-    xlst.sort_unstable();
-    ylst.sort_unstable();
-    zlst.sort_unstable();
-    let nx = xlst.len();
-    let ny = ylst.len();
-    let nz = zlst.len();
-
-    let mut small_cubes = Vec::with_capacity((nx - 1) * (ny - 1) * (nz - 1));
-    for i in 1..nx {
-        for j in 1..ny {
-            for k in 1..nz {
-                small_cubes.push((
-                    xlst[i - 1],
-                    xlst[i],
-                    ylst[j - 1],
-                    ylst[j],
-                    zlst[k - 1],
-                    zlst[k],
-                ));
-            }
-        }
-    }
-
-    let m = small_cubes.len();
-    let mut state_small = vec![false; m];
-    for i in 0..n {
-        for j in 0..m {
-            if state_lst[i] != state_small[j] {
-                if small_cubes[j].0 >= cube_lst[i][0]
-                    && small_cubes[j].1 - 1 <= cube_lst[i][1]
-                    && small_cubes[j].2 >= cube_lst[i][2]
-                    && small_cubes[j].3 - 1 <= cube_lst[i][3]
-                    && small_cubes[j].4 >= cube_lst[i][4]
-                    && small_cubes[j].5 - 1 <= cube_lst[i][5]
-                {
-                    state_small[j] = state_lst[i];
-                }
-            }
+        if need {
+            cube_lst.push(Cube::from_sp(pos, state));
         }
     }
 
     let mut count = 0;
-    for j in 0..m {
-        if state_small[j] {
-            count += (small_cubes[j].1 - small_cubes[j].0) as usize
-                * (small_cubes[j].3 - small_cubes[j].2) as usize
-                * (small_cubes[j].5 - small_cubes[j].4) as usize;
+    for (i, cube) in cube_lst.iter().enumerate() {
+        let mut left = vec![*cube];
+        let mut j = i;
+        while j > 0 {
+            j -= 1;
+            let mut left2 = vec![];
+            for temp in left.iter() {
+                let (delta, other) = temp.intersect(&cube_lst[j]);
+                left2.extend(other);
+                count += delta;
+            }
+            left = left2;
+            if left.is_empty() {
+                break;
+            }
+        }
+        if cube.on {
+            for cube in left {
+                count += cube.count();
+            }
         }
     }
-    count
+    count as usize
 }
 
 pub fn part2() -> usize {
@@ -117,77 +163,45 @@ pub fn part2() -> usize {
     )
     .unwrap();
     let n = lines.len();
-    let mut state_lst = Vec::with_capacity(n);
-    let mut cube_lst: Vec<Vec<i32>> = Vec::with_capacity(n);
+    let mut cube_lst: Vec<Cube> = Vec::with_capacity(n);
     for line in lines {
         let caps = re.captures(&line).unwrap();
-        state_lst.push(if caps.get(1).unwrap().as_str() == "on" {
-            true
-        } else {
-            false
-        });
-        cube_lst.push(
-            (2..8)
-                .into_iter()
-                .map(|i| caps.get(i).unwrap().as_str().parse().unwrap())
-                .collect(),
-        );
-    }
-
-    let mut xset = HashSet::new();
-    let mut yset = HashSet::new();
-    let mut zset = HashSet::new();
-
-    for cube in &cube_lst {
-        xset.insert(cube[0]);
-        xset.insert(cube[1] + 1);
-        yset.insert(cube[2]);
-        yset.insert(cube[3] + 1);
-        zset.insert(cube[4]);
-        zset.insert(cube[5] + 1);
-    }
-
-    let mut xlst: Vec<i32> = xset.into_iter().collect();
-    let mut ylst: Vec<i32> = yset.into_iter().collect();
-    let mut zlst: Vec<i32> = zset.into_iter().collect();
-    xlst.sort_unstable();
-    ylst.sort_unstable();
-    zlst.sort_unstable();
-    let nx = xlst.len();
-    let ny = ylst.len();
-    let nz = zlst.len();
-
-    let mut state_small = vec![vec![vec![false; nz - 1]; ny - 1]; nx - 1];
-    for m in 0..n {
-        for i in 0..nx - 1 {
-            if xlst[i] < cube_lst[m][0] || xlst[i + 1] - 1 > cube_lst[m][1] {
-                continue;
-            }
-            for j in 0..ny - 1 {
-                if ylst[j] < cube_lst[m][2] || ylst[j + 1] - 1 > cube_lst[m][3] {
-                    continue;
-                }
-                for k in 0..nz - 1 {
-                    if zlst[k] < cube_lst[m][4] || zlst[k + 1] - 1 > cube_lst[m][5] {
-                        continue;
-                    }
-                    state_small[i][j][k] = state_lst[m];
-                }
-            }
-        }
+        let state = caps.get(1).unwrap().as_str() == "on";
+        let pos: Vec<T> = (2..8)
+            .into_iter()
+            .map(|i| caps.get(i).unwrap().as_str().parse::<T>().unwrap())
+            .collect();
+        cube_lst.push(Cube::from_sp(pos, state));
     }
 
     let mut count = 0;
-    for i in 0..nx - 1 {
-        for j in 0..ny - 1 {
-            for k in 0..nz - 1 {
-                if state_small[i][j][k] {
-                    count += (xlst[i + 1] - xlst[i]) as usize
-                        * (ylst[j + 1] - ylst[j]) as usize
-                        * (zlst[k + 1] - zlst[k]) as usize;
-                }
+    for (i, cube) in cube_lst.iter().enumerate() {
+        let mut left = vec![*cube];
+        let mut j = i;
+        while j > 0 {
+            j -= 1;
+            let mut left2 = vec![];
+            for temp in left.iter() {
+                let (delta, other) = temp.intersect(&cube_lst[j]);
+                left2.extend(other);
+                count += delta;
+            }
+            left = left2;
+            if left.is_empty() {
+                break;
+            }
+        }
+        if cube.on {
+            for cube in left {
+                count += cube.count();
             }
         }
     }
-    count
+    count as usize
+}
+
+#[test]
+fn test_22() {
+    assert_eq!(607657, part1());
+    assert_eq!(1187742789778677, part2());
 }
